5875a4a0d0d7e3b8c3ec5f280d8a92f066d10792
suryajayantara <ptsuryajayantara01@gmail.com>
2022-05-19 12:11:28 +0800

    :smile: make seeders + fakers

---
+++ warebook_student_laravel/database/seeders/MahasiswaSeeders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class MahasiswaSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $faker = Faker::create('id_ID');
+        for ($i=0; $i < 10 ; $i++) { 
+            // Foreach here 
+        }
+        
+    }
+}
